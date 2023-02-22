aa999c2a3bc61b438e72ce77bca8d7b9d2937641
Nachiket Naganure <nachiket@client-10-228-18-64.tamulink.tamu.edu>
2023-02-22 14:57:02 -0600

    Adding new movie titles to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Shawshank Redemption', :rating => 'PG-13',
+    :release_date => '16-Apr-1995'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Nov-2010'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '30-May-2014'},
+  {:title => 'Fury', :rating => 'R',
+    :release_date => '19-Feb-2014'}
 ]
 
 more_movies.each do |movie|
